67bc7945081d67316e84a85d7e021eecd06deb68
Shamim <shamimmaloba@gmail.com>
2021-12-12 07:23:15 +0300

    added const my first element h1

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+const myfirstelement=<h1>ecomart shopping</h1>
 ReactDOM.render(
   <React.StrictMode>
     <App />
