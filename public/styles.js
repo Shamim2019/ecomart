a847ab56e7a6a83e50bdbe8f6da4a5ea77a6b5c9
Shamim <shamimmaloba@gmail.com>
2021-12-12 17:23:27 +0300

    styled header

---
+++ public/styles.js
@@ -0,0 +1,16 @@
+const Header = {
+    padding: "10px 20px",
+    textAlign: "center",
+    color: "white",
+    fontSize: "22px"
+  }
+  const ErrorMessage = {
+    color: "white",
+    fontSize: "13px"
+  }
+  
+  const styles = {
+    TodoComponent: TodoComponent,
+    Header: Header,
+    ErrorMessage: ErrorMessage
+  }
